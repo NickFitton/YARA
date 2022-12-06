06d4869c3357f7d81e18d4cd76474bb0579fe6d0
Nick Fitton <nick.fitton@ovo.com>
2022-12-06 17:25:39 +0000

    Re-init and add new icons

--- ios/yaraTests/yaraTests.m
+++ ios/yaraTests/yaraTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface yaraTests : XCTestCase
+@interface YaraTests : XCTestCase
 
 @end
 
-@implementation yaraTests
+@implementation YaraTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
