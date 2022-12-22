3b94216542285a4b2c180c628f254f8d27e6305a
Nick Fitton <nick.fitton@ovo.com>
2022-12-22 16:41:24 +0000

    Sizing up scroll items

--- src/stack/journeys/makeRecipe/screens/Method.tsx
+++ src/stack/journeys/makeRecipe/screens/Method.tsx
@@ -1,13 +1,102 @@
-import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {MethodModel} from '../../../../db/models/Method';
+import {useRecipe} from '../../../../db/recipeHooks';
+import {LoadingSpinner} from '../components/LoadingSpinner';
+import {MakeRecipeProps} from '../types';
 
-export function MethodScreen() {
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    color: '#111',
+    fontSize: 32,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  landscapeContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+  },
+  portraitContainer: {
+    flexDirection: 'column',
+  },
+  landscapeText: {
+    flexGrow: 1,
+  },
+});
+
+export function MethodScreen({route, navigation}: MakeRecipeProps<'Method'>) {
+  const recipeData = useRecipe(route.params.id);
+  const [{width, height}, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({width: -1, height: -1});
+  const ref = useRef<View>(null);
+
+  useEffect(() => {
+    const refreshSlideSize = () =>
+      setTimeout(
+        () =>
+          ref.current?.measureInWindow((_x, _y, refWidth, refHeight) =>
+            setDimensions({width: refWidth, height: refHeight}),
+          ),
+        100,
+      );
+    refreshSlideSize();
+    const subscription = Dimensions.addEventListener(
+      'change',
+      refreshSlideSize,
+    );
+    return () => subscription?.remove();
+  }, []);
+
+  useEffect(() => {
+    const headerRight = () => <Button title="Next" />;
+    navigation.setOptions({headerRight});
+  }, [navigation]);
+
+  if (recipeData.status === 'loading') {
+    return <LoadingSpinner message="Loading recipe ingredients" />;
+  }
+
+  const renderItem = ({item: method}: {item: MethodModel}) => {
+    const isLandscape = width > height;
+    return (
+      <View style={[{width, height, maxWidth: width, maxHeight: height}]}>
+        <View
+          style={[
+            styles.container,
+            isLandscape ? styles.landscapeContainer : styles.portraitContainer,
+          ]}>
+          <Text style={[styles.text, isLandscape && styles.landscapeText]}>
+            {method.step}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  const keyExtractor = ({id}: MethodModel): string => id!;
+
   return (
-    <ScrollView>
-      <Text>1</Text>
-      <Text>2</Text>
-      <Text>3</Text>
-      <Text>4</Text>
-    </ScrollView>
+    <>
+      <View style={StyleSheet.absoluteFill} ref={ref} collapsable={false} />
+      <FlatList
+        pagingEnabled
+        renderItem={renderItem}
+        data={recipeData.data?.method}
+        keyExtractor={keyExtractor}
+      />
+    </>
   );
 }
--- src/stack/journeys/makeRecipe/screens/MiseEnPlace.tsx
+++ src/stack/journeys/makeRecipe/screens/MiseEnPlace.tsx
@@ -1,59 +1,117 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
+  Button,
   Dimensions,
+  FlatList,
   Image,
-  ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import {IngredientModel} from '../../../../db/models/Ingredient';
 import {useRecipe} from '../../../../db/recipeHooks';
 import {LoadingSpinner} from '../components/LoadingSpinner';
 import {MakeRecipeProps} from '../types';
 
 const styles = StyleSheet.create({
-  textContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   text: {
     textAlign: 'center',
     color: '#111',
     fontSize: 32,
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  landscapeContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+  },
+  portraitContainer: {
+    flexDirection: 'column',
+  },
+  landscapeText: {
+    flexGrow: 1,
+  },
 });
 
-export function MiseEnPlaceScreen({route}: MakeRecipeProps<'MiseEnPlace'>) {
-  const {width} = Dimensions.get('window');
+export function MiseEnPlaceScreen({
+  route,
+  navigation,
+}: MakeRecipeProps<'MiseEnPlace'>) {
   const recipeData = useRecipe(route.params.id);
+  const [{width, height}, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({width: -1, height: -1});
+  const ref = useRef<View>(null);
+
+  useEffect(() => {
+    const refreshSlideSize = () =>
+      setTimeout(
+        () =>
+          ref.current?.measureInWindow((_x, _y, refWidth, refHeight) =>
+            setDimensions({width: refWidth, height: refHeight}),
+          ),
+        100,
+      );
+    refreshSlideSize();
+    const subscription = Dimensions.addEventListener(
+      'change',
+      refreshSlideSize,
+    );
+    return () => subscription?.remove();
+  }, []);
+
+  useEffect(() => {
+    const headerRight = () => (
+      <Button
+        title="Next"
+        onPress={() => navigation.navigate('Method', {id: route.params.id})}
+      />
+    );
+    navigation.setOptions({headerRight});
+  }, [navigation, route.params.id]);
 
   if (recipeData.status === 'loading') {
     return <LoadingSpinner message="Loading recipe ingredients" />;
   }
 
-  return (
-    <ScrollView
-      horizontal
-      pagingEnabled
-      style={{
-        flex: 1,
-        flexDirection: 'row',
-      }}>
-      {recipeData.data?.ingredients.map(ingredient => (
-        <View style={[styles.textContainer, {width}]}>
+  const renderItem = ({item: ingredient}: {item: IngredientModel}) => {
+    const isLandscape = width > height;
+    return (
+      <View style={[{width, height, maxWidth: width, maxHeight: height}]}>
+        <View
+          style={[
+            styles.container,
+            isLandscape ? styles.landscapeContainer : styles.portraitContainer,
+          ]}>
           <Image
             source={{uri: 'http://placekitten.com/300/300'}}
             style={{width: 300, height: 300}}
           />
-          <Text style={styles.text}>
+          <Text style={[styles.text, isLandscape && styles.landscapeText]}>
             {ingredient.type === 'raw'
               ? ingredient.ingredient
               : ingredient.name}
           </Text>
         </View>
-      ))}
-    </ScrollView>
+      </View>
+    );
+  };
+
+  const keyExtractor = (ingredient: IngredientModel): string => ingredient.id!;
+
+  return (
+    <>
+      <View style={StyleSheet.absoluteFill} ref={ref} collapsable={false} />
+      <FlatList
+        pagingEnabled
+        renderItem={renderItem}
+        data={recipeData.data?.ingredients}
+        keyExtractor={keyExtractor}
+      />
+    </>
   );
 }
