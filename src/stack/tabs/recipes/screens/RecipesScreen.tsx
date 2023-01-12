bee1238bda5b1cd8b0dad287d9f05185988fb37e
Nick Fitton <nick.fitton@ovo.com>
2023-01-12 14:59:55 +0000

    Cleanup recipes

--- src/stack/tabs/recipes/screens/RecipesScreen.tsx
+++ src/stack/tabs/recipes/screens/RecipesScreen.tsx
@@ -1,113 +1,74 @@
 import {NavigationProp, useNavigation} from '@react-navigation/native';
-import React, {PropsWithChildren} from 'react';
-import {View, TouchableOpacity, FlatList} from 'react-native';
-import {Card, Paragraph, ActivityIndicator, Text} from 'react-native-paper';
+import React, {useState} from 'react';
+import {View, FlatList} from 'react-native';
+import {List, Searchbar} from 'react-native-paper';
+import {ErrorFiller} from '../../../../components/data/Error/Error';
+import {LoadingFiller} from '../../../../components/data/Loading/Loading';
 import {FAB} from '../../../../components/FAB/FAB';
 
 import {Screen} from '../../../../components/Screen/Screen';
-import {useRecipe, useRecipes} from '../../../../db/hooks/recipeHooks';
+import {useRecipes} from '../../../../db/hooks/recipeHooks';
+import {RecipePreview} from '../../../../db/models/Recipe';
+import {useDebounce} from '../../../../utils/hooks/useDebounce';
 import {useCreateRecipeJourney} from '../../../journeys/createRecipe/utils';
 import {RecipeStackParamList} from '../RecipeStackParam';
 
-function RecipePreviewWrapper({id, children}: PropsWithChildren<{id: string}>) {
+const listItemRightArrow = () => <List.Icon icon="chevron-right" />;
+
+function RecipeListItem({
+  recipe: {id, name, description},
+}: {
+  recipe: RecipePreview;
+}) {
   const navigation =
     useNavigation<NavigationProp<RecipeStackParamList, 'RecipesRoot'>>();
-
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate({name: 'View Recipe', params: {id}})}>
-      <Card>{children}</Card>
-    </TouchableOpacity>
+    <List.Item
+      title={name}
+      description={description}
+      onPress={() => navigation.navigate({name: 'View Recipe', params: {id}})}
+      right={listItemRightArrow}
+    />
   );
 }
 
-function RecipePreview({id}: {id: string}) {
-  const data = useRecipe(id);
+function RecipeFlatList() {
+  const [search, setSearch] = useState('');
+  const debouncedQuery = useDebounce(search, 750);
+  const data = useRecipes(debouncedQuery);
 
   switch (data.status) {
+    case 'loading':
+      return <LoadingFiller subject="books" />;
     case 'error':
-      return (
-        <RecipePreviewWrapper id={id}>
-          <Text>!!!</Text>
-        </RecipePreviewWrapper>
-      );
+      return <ErrorFiller subject="books" error={data.error} />;
     case 'success':
       return (
-        <RecipePreviewWrapper id={id}>
-          <Card.Title title={data.data.name} />
-          {data.data.description ? (
-            <Card.Content>
-              <Paragraph>{data.data.description}</Paragraph>
-            </Card.Content>
-          ) : null}
-        </RecipePreviewWrapper>
-      );
-    case 'loading':
-    default:
-      return (
-        <RecipePreviewWrapper id={id}>
-          <ActivityIndicator size="large" />
-        </RecipePreviewWrapper>
+        <>
+          <View style={{padding: 8}}>
+            <Searchbar value={search} onChangeText={setSearch} />
+          </View>
+          <FlatList
+            refreshing={data.isRefetching}
+            onRefresh={() => {
+              data.refetch().catch(console.error);
+            }}
+            data={data.data}
+            renderItem={({item}) => <RecipeListItem recipe={item} />}
+          />
+        </>
       );
   }
-}
-
-function Seperator() {
-  return <View style={{height: 8}} />;
-}
-
-function ListEmpty() {
-  return <Text variant="bodyLarge">You don&apos;t have any recipes yet</Text>;
 }
 
 export function RecipesScreen() {
   const startCreateRecipeJourney = useCreateRecipeJourney();
-  const data = useRecipes();
-
-  switch (data.status) {
-    case 'error':
-      return (
-        <Screen
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text variant="bodyLarge">Something went wrong</Text>
-        </Screen>
-      );
-    case 'success':
-      return (
-        <>
-          <Screen>
-            <FlatList
-              refreshing={data.isRefetching}
-              onRefresh={() => {
-                data.refetch().catch(console.error);
-              }}
-              ItemSeparatorComponent={Seperator}
-              ListEmptyComponent={ListEmpty}
-              data={data.data}
-              renderItem={({item: {id}}) => <RecipePreview id={id} />}
-            />
-          </Screen>
-          <FAB icon="plus" onPress={startCreateRecipeJourney} />
-        </>
-      );
-    case 'loading':
-    default:
-      return (
-        <Screen
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <ActivityIndicator size="large" />
-          <Text variant="bodyLarge">Loading your recipes</Text>
-        </Screen>
-      );
-  }
+  return (
+    <>
+      <Screen style={{padding: 0, flex: 1, flexDirection: 'column'}}>
+        <RecipeFlatList />
+      </Screen>
+      <FAB icon="plus" onPress={startCreateRecipeJourney} />
+    </>
+  );
 }
