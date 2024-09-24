1c0b5ec053104e2e100bbf7c52472001d1d22283
Nick Fitton <nick@humaans.io>
2024-09-24 20:18:16 +0100

    Generate recipes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [],
+  imports: [RecipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
