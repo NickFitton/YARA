c880a3ebb9a741c064c7889cb450faa183f23bb9
Nick Fitton <nick@humaans.io>
2024-09-29 13:39:50 +0100

    Bad privacy policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["md", "mdx", "tsx"],
+};
+
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
