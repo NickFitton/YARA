21a9d4811f72c8d647dd9cbf02fe6bfbe6b35cd7
Nick Fitton <nick@humaans.io>
2024-10-03 16:44:28 +0100

    user and recipe creation 🥳

--- src/app/login/loginForm.tsx
+++ src/app/login/loginForm.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-import { useLogin } from "./login.hook";
 import { useForm } from "react-hook-form";
 import { loginSchema, LoginSchema } from "./login.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -21,8 +20,8 @@ import { attemptLogin } from "./login.action";
 import { useState } from "react";
 
 export const LoginForm = () => {
-const [isPending, setIsPending] = useState(false);
-  const router = useRouter()
+  const [isPending, setIsPending] = useState(false);
+  const router = useRouter();
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -35,57 +34,55 @@ const [isPending, setIsPending] = useState(false);
     // I'd like to be able to tanstack this
     const response = await attemptLogin(data);
     if ("accessToken" in response && response.accessToken) {
-      router.push('/dashboard')
+      router.push("/dashboard");
     }
     setIsPending(false);
   };
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email address</FormLabel>
-                    <FormControl>
-                      <Input type="email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              ></FormField>
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              ></FormField>
-            </div>
-            <div className="flex flex-row gap-3 justify-between items-center">
-              <Link
-                className="w-32 text-sm hover:underline underline-offset-4 flex flex-row gap-1 items-center"
-                href="/sign-up"
-              >
-                Sign up
-              </Link>
-              <Button className="w-32" type="submit" disabled={isPending}>
-                {isPending ? <Hourglass size="1rem" /> : "Sign in"}
-              </Button>
-            </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email address</FormLabel>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
           </div>
-        </form>
-      </Form>
-    </>
+          <div className="flex flex-row gap-3 justify-between items-center">
+            <Link
+              className="w-32 text-sm hover:underline underline-offset-4 flex flex-row gap-1 items-center"
+              href="/sign-up"
+            >
+              Sign up
+            </Link>
+            <Button className="w-32" type="submit" disabled={isPending}>
+              {isPending ? <Hourglass size="1rem" /> : "Sign in"}
+            </Button>
+          </div>
+        </div>
+      </form>
+    </Form>
   );
 };
---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("accessToken");
+  if (!cookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  try {
+    const expDate = new Date(
+      JSON.parse(atob(cookie.value.split(".")[1])).exp * 1000
+    ).getTime();
+    if (expDate < Date.now()) {
+      request.cookies.delete("accessToken");
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  } catch (e: unknown) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/dashboard'],
+};
