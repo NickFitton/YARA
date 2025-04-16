d8917d1db9b1fc170d662de3fe936268f5b493d5
Nick Fitton <nick@humaans.io>
2025-04-16 09:11:06 +0100

    Fix middleware, setup select box

--- apps/web/src/app/recipes/[id]/edit/BasicInformation.tsx
+++ apps/web/src/app/recipes/[id]/edit/BasicInformation.tsx
@@ -114,11 +114,47 @@ function TextAreaField<
 function SelectField<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
->({ placeholder = "", ...fieldProps }: FieldProps<TFieldValues, TName>) {
+>({
+  control,
+  name,
+  Icon,
+  label,
+}: {
+  label: string;
+  Icon?: ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+  >;
+  name: TName;
+  control: Control<TFieldValues>;
+  placeholder?: string;
+}) {
   return (
-    <Field
-      {...fieldProps}
-      render={({ field }) => <Textarea placeholder={placeholder} {...field} />}
+    <FormField
+      control={control}
+      name={name}
+      render={({ field, fieldState, formState }) => (
+        <FormItem>
+          <FormLabel className="flex gap-1 items-center">
+            {Icon ? <Icon className="w-4 h-4 text-orange-500" /> : null}
+            {label}
+          </FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder="1" />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {new Array(5).fill(0).map((_, i) => (
+                <SelectItem key={i} value={i + 1 + ""}>
+                  {i + 1}{" "}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
     />
   );
 }
@@ -192,7 +228,7 @@ export default function BasicInformationEditor({ recipe }: RecipePageData) {
               label="Cook Time"
               Icon={ChefHat}
             />
-            <TextField
+            <SelectField
               control={form.control}
               name="servings"
               label="Servings"
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/recipes", "/recipes/:id*"],
+  matcher: ["/"],
 };
